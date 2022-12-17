48f9d0b0874bcd88cd1618fd5c88f39342750798
kristen500 <115898319+kristen500@users.noreply.github.com>
2022-12-16 20:45:16 -0800

    Add project files.

---
+++ ProductCatalogAPI/Data/CatalogContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCatalogAPI.Domain;
+
+namespace ProductCatalogAPI.Data
+{
+    public class CatalogContext : DbContext
+    {
+        public DbSet<CatalogType> CatalogTypes { get; set; }    
+        public DbSet<CatalogBrand> CatalogBrands { get; set; }  
+        public DbSet<CatalogItem> Catalog { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CatalogType>(e => )
+        }
+    }
+}
