de1d1b443e7cdbc0d62ab76a7488f116d1d9cc3e
kristen500 <115898319+kristen500@users.noreply.github.com>
2022-12-18 08:41:53 -0800

    working updates

---
+++ ProductCatalogAPI/Controllers/CatalogController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductCatalogAPI.Data;
+using ProductCatalogAPI.Domain;
+
+namespace ProductCatalogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+
+        private readonly CatalogContext _context;
+        private readonly IConfiguration _config;
+        public CatalogController(CatalogContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+        
+        [HttpGet("[action")]
+        public async Task<IActionResult> CatalogTypes()
+        {
+          var types = await _context.CatalogTypes.ToListAsync();
+            return Ok(types);
+        }
+
+        [HttpGet("[action")]
+        public async Task<IActionResult> Catalogbrands()
+        {
+            var Brands = await _context.CatalogBrands.ToListAsync();
+            return Ok(Brands);
+        }
+
+        [HttpGet("[action}")]
+        public async Task<IActionResult> Items(
+            [FromQuery]int pageIndex = 0, [FromQuery]int pageSize = 6)
+        {
+          var items= await  _context.Catalog
+                .OrderBy(c => c.Name)
+                .Skip(pageIndex * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            items = ChangePictureUrl(items);
+            return Ok(items);   
+        }
+
+        private List<CatalogItem> ChangePictureUrl(List<CatalogItem> items)
+        {
+            items.ForEach(item => item.PictureUrl = item.PictureUrl
+                                        .Replace("http://externalcatalogbaseurltobereplaced",
+                                        _config["ExternalBaseUrl"]));
+            return items;
+        }
+    }
+}
--- ProductCatalogAPI/Program.cs
+++ ProductCatalogAPI/Program.cs
@@ -1,13 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using ProductCatalogAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<CatalogContext>(
+                options => options.UseSqlServer(configuration["ConnectionString"]));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProviders = scope.ServiceProvider;
+    var context = serviceProviders.GetRequiredService<CatalogContext>();
+    CatalogSeed.Seed(context);
+}
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
