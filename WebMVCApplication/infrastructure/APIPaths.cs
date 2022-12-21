94f5e0bb59582a843bd51fde24b2f5dc72bcf319
kristen500 <115898319+kristen500@users.noreply.github.com>
2022-12-20 19:03:50 -0800

    continue working

---
+++ WebMVCApplication/infrastructure/APIPaths.cs
@@ -0,0 +1,18 @@
+﻿namespace WebMVCApplication.infrastructure
+{
+    public static class APIPaths
+    {
+        public static string GetAllTypes(string baseUrl)
+        {
+            return $"{baseUrl}/eventtypes";
+        }
+        public static string GetAllOrganizers(string baseUrl)
+        {
+            return $"{baseUrl}/eventorganizers";
+        }
+        public static string GetAllItems(string baseUrl, int page, int take)
+        {
+            return $"{baseUrl}/items?pageIndex={page}&pageSize={take}";
+        }
+    }
+}
