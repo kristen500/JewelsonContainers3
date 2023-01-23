919bf9e97a47da66eb47aabff68558e1307ab712
kristen500 <115898319+kristen500@users.noreply.github.com>
2023-01-22 22:01:20 -0800

    update

--- WebMVC/infrastructure/CustomHttpClient.cs
+++ WebMVC/infrastructure/CustomHttpClient.cs
@@ -10,19 +10,19 @@ namespace WebMVC.infrastructure
         {
             _httpClient = new HttpClient();
         }
-        public async Task<HttpResponseMessage> DeleteAsync(string uri, string authorizationToken = null, string authorizationMethod = "Bearer")
+        public async Task<HttpResponseMessage> DeleteAsync<T>(string uri, T item, string authorizationToken = null, string authorizationMethod = "Bearer")
         {
             var requestMessage = new HttpRequestMessage(HttpMethod.Delete, uri);
             if (authorizationToken != null)
             {
-                requestMessage.Headers.Authorization = new AuthenticationHeaderValue(authorizationMethod,
-                    authorizationToken);
+                requestMessage.Headers.Authorization = new
+                    AuthenticationHeaderValue(authorizationMethod, authorizationToken);
             }
             return await _httpClient.SendAsync(requestMessage);
+
         }
 
-        public async Task<string> GetStringAsync(string uri,
-            string authorizationToken = null, string authorizationMethod = "Bearer")
+        public async Task<string> GetStringAsync(string uri, string authorizationToken = null, string authorizationMethod = "Bearer")
         {
             var requestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
             if (authorizationToken != null)
@@ -34,20 +34,18 @@ namespace WebMVC.infrastructure
             return await response.Content.ReadAsStringAsync();
         }
 
-        public async Task<HttpResponseMessage> PostAsync<T>(string uri, T item,
-            string authorizationToken = null, string authorizationMethod = "Bearer")
+        public async Task<HttpResponseMessage> PostAsync<T>(string uri, T item, string authorizationToken = null, string authorizationMethod = "Bearer")
         {
-            return await DoPostPutAysnc(HttpMethod.Post, uri, item, authorizationToken, authorizationMethod);
+            return await DoPostPutAsync(HttpMethod.Post, uri, item, authorizationToken, authorizationMethod);
         }
 
-        public async Task<HttpResponseMessage> PutAsync<T>(string uri, T item,
-            string authorizationToken = null, string authorizationMethod = "Bearer")
+        public async Task<HttpResponseMessage> PutAsync<T>(string uri, T item, string authorizationToken = null, string authorizationMethod = "Bearer")
         {
-            return await DoPostPutAysnc(HttpMethod.Put, uri, item, authorizationToken, authorizationMethod);
+            return await DoPostPutAsync(HttpMethod.Put, uri, item, authorizationToken, authorizationMethod);
         }
 
-        private async Task<HttpResponseMessage> DoPostPutAysnc<T>(HttpMethod method, string uri,
-            T item, string authorizationToken, string authorizationMethod)
+        private async Task<HttpResponseMessage> DoPostPutAsync<T>
+            (HttpMethod method, string uri, T item, string authorizationToken, string authorizationMethod)
         {
             var requestMessage = new HttpRequestMessage(method, uri);
             requestMessage.Content = new StringContent(JsonConvert.SerializeObject(item),
